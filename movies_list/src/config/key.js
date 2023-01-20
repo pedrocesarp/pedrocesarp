7283dcdfb1ef4c3c52501e1f9d3b35cd88b0b1b1
Pedro César <pedrocesar5@gmail.com>
2023-01-20 18:05:33 -0300

    Delete movies_list directory

--- movies_list/src/config/key.js
+++
@@ -1,2 +0,0 @@
-
-export const Keyapi = "7b135a13b80db570c196072fa87cd19c"
--- movies_list/src/services/getDayMovies.js
+++
@@ -1,9 +0,0 @@
-import { Keyapi } from "../config/key";
-
-export const getDayMovies = (setMovies, page ) => {
-
-    fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${Keyapi}&language=en-US&page=${page}`)
-    .then (response=> response.json())
-    .then (data => setMovies (data.results))
-
-}
